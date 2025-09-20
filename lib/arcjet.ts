479af50765c711d9cbd392fd80c69a6d867db64e
vinryde <vinryde66@gmail.com>
2025-09-20 18:06:32 +0530

    done

---
+++ lib/arcjet.ts
@@ -0,0 +1,18 @@
+import arcjet, { detectBot, fixedWindow, protectSignup,sensitiveInfo,shield,slidingWindow } from "@arcjet/next";
+
+export{
+    detectBot, fixedWindow, protectSignup,sensitiveInfo,shield,slidingWindow
+};
+
+export default arcjet({
+    key: process.env.ARCJET_KEY as string,
+    characteristics: ["fingerprint"],
+
+    rules:[
+        shield  ({
+            mode: 'LIVE'
+        }) 
+    ]
+
+})
+
